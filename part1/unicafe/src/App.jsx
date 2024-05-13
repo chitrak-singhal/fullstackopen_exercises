8de9a0bcc75ef152b2fc644d7be03a822a89801f
chitrak-singhal <chitrak.singhal.eee21@itbhu.ac.in>
2024-05-13 18:20:17 +0530

    Part 1.7-1.8 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -23,6 +23,19 @@ function increment(value, func){
   func(upd_value);
 }
 
+function Stats({good, bad, neutral}){
+  const all = (good+bad+neutral)
+  const avg = (good*1+bad*-1)/all
+  const pos = (good)/all
+  return (
+    <>
+        <p> all {all}</p>
+        <p>Average {avg}</p>
+        <p> Positive {pos}</p>
+    </>
+  )
+}
+
 function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -40,6 +53,8 @@ function App() {
       <Display text ="good" value={good} />
       <Display text ="neutral" value={neutral} />
       <Display text ="bad" value={bad} />
+      
+      <Stats good={good} bad ={bad} neutral ={neutral} />
     </div>
   )
 }
