79e04e2971a8803ca650d048273ab0bb88bd150d
chitrak-singhal <chitrak.singhal.eee21@itbhu.ac.in>
2024-05-14 16:56:53 +0530

    part 2.8 done

--- part1/phonebook/src/App.jsx
+++ part1/phonebook/src/App.jsx
@@ -2,33 +2,48 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '9547123655' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
 
   const addName = (event)=>{
     event.preventDefault()
     //console.log('Submitted',event.target);
     const new_name = {
-      name : newName
+      name : newName,
+      number: newNum
+
     }
     console.log(persons)
     if (persons.findIndex((e)=>e.name==new_name.name)===-1)
     {
       // console.log(persons.findIndex((e)=>e==new_name));
       // console.log(new_name);
-      setPersons(persons.concat(new_name))
+      if (persons.findIndex((e)=>e.number==new_name.number)!==-1)
+        {
+          {
+            alert(`${newNum} is already added to the phonebook`)
+          }
+        }
+        else
+          setPersons(persons.concat(new_name))
     }
     else
     {
       alert(`${newName} is already added to the phonebook`)
     }
     setNewName('')
+    setNewNum('')
   }
 
   const handleNameChange=(event)=>{
     //console.log(event.target.value)
     setNewName(event.target.value)
+  }
+  const handleNumChange=(event)=>{
+    //console.log(event.target.value)
+    setNewNum(event.target.value)
   }
 
   return (
@@ -39,11 +54,14 @@ const App = () => {
           name: <input value ={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value ={newNum} onChange={handleNumChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person=><p key={person.name}>{person.name}</p>)}
+      {persons.map(person=><p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
